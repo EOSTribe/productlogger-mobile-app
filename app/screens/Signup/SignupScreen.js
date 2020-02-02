9de4c0329fa0df97a49bed3e92a021cdbfdb3dee
snsoft221 <juliafodor221@gmail.com>
2020-02-02 05:00:09 +0200

    Setup eosjs module

--- app/screens/Signup/SignupScreen.js
+++ app/screens/Signup/SignupScreen.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import { View, TextInput, TouchableOpacity, Text } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
+
+import { Api, JsonRpc, RpcError, } from 'eosjs-rn';
+import { JsSignatureProvider } from 'eosjs-rn/dist/eosjs-jssig';
+import { TextEncoder, TextDecoder } from 'text-encoding';
+
 import ecc from 'eosjs-ecc-rn';
 
 import styles from './SignupScreen.style';
@@ -16,6 +21,48 @@ const SignupScreen = props => {
 
     console.log('Private key: ', privateKey);
     console.log('Public key: ', publicKey);
+
+    const signatureProvider = new JsSignatureProvider([
+      '5JfZYEuiiDTBmyNBLTAjU4A8uBzrjFVU5cgKaiBLtF5TpwNQzNs',
+    ]);
+    const rpc = new JsonRpc('http://testnet.telos.eostribe.io');
+    const api = new Api({
+      rpc,
+      signatureProvider,
+      textDecoder: new TextDecoder(),
+      textEncoder: new TextEncoder(),
+    });
+
+    try {
+      const result = await api.transact(
+        {
+          actions: [
+            {
+              account: 'productloger',
+              name: 'addmanager',
+              authorization: [
+                {
+                  actor: 'productloger',
+                  permission: 'active',
+                },
+              ],
+              data: {
+                manager: 'juliafodor12',
+                description: 'manager 3',
+              },
+            },
+          ],
+        },
+        {
+          blocksBehind: 3,
+          expireSeconds: 30,
+        },
+      );
+
+      console.log(result);
+    } catch (err) {
+      console.log('Transaction error', err);
+    }
 
     // props.navigation.navigate('Manager');
   };
